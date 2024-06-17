93c8836ceadbf8c193e39970ae60b7aa74cc6fab
sc-exo <qiushijsxs@stu.xmu.edu.cn>
2024-06-17 09:50:33 +0000

    used to change to modified nvme ko

---
+++ scripts/nvme_module_change.sh
@@ -0,0 +1,48 @@
+#!/bin/bash  
+  
+# 检查参数个数  
+if [ "$#" -ne 1 ]; then  
+    echo "Usage: $0 <origin|share>"  
+    exit 1  
+fi  
+path="../kernel_module/nvme/host"
+KDIR="/lib/modules/$(uname -r)/kernel/drivers/nvme/host"
+# 定义函数来卸载和加载模块  
+
+unload_modules() { 
+    rmmod nvme 
+    rmmod nvme-core 
+}  
+
+unload_modified_modules() {
+    rmmod snvme  
+    rmmod snvme-core
+}  
+
+load_custom_modules() {
+    local path=$1    
+    insmod $path/nvme-core.ko  
+    insmod $path/nvme.ko  
+}  
+load_modified_modules() {  
+    local path=$1  
+    insmod $path/snvme-core.ko  
+    insmod $path/snvme.ko  
+}  
+
+# 根据参数决定操作  
+case "$1" in  
+    "share")  
+        unload_modules  
+        load_modified_modules $path 
+        ;;  
+    "origin")  
+        unload_modified_modules  
+        # 注意：这里假设了内核版本为5.15，根据实际情况可能需要调整  
+        load_custom_modules $KDIR  
+        ;;  
+    *)  
+        echo "Invalid parameter. Usage: $0 <origin|share>"  
+        exit 1  
+        ;;  
+esac
