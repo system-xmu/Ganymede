3deab68c7bae697548d9c45e7e1a908d2ca7f534
Weinan Liu <wnliu@stu.xmu.edu.cn>
2024-08-20 16:04:09 +0000

    gpu pagecache: ok18

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+
+# 1. For libnvm.so
+mkdir -p build
+cd build
+cmake ..
+make -j
+make bench_test -j
+cd ..
+
+# 2. For libgeminiFs.a
+cd libgeminiFs_src
+make -j
+cd ..
+cp libgeminiFs_src/libgeminiFs.a ./build/lib
+
+
+# 3. For examples
+cd examples
+make -j
