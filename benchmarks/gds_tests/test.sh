3d05571594ad73dba433043ade1e3957b0e3549d
hyf <949973766@qq.com>
2024-08-22 02:20:22 +0000

    upload test.sh for gds

---
+++ benchmarks/gds_tests/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+THREADS=(1 4  8 16 32 64 128 256 512)  
+for threads in "${THREADS[@]}"  
+do  
+    logfile="test_gds_cuFileRead_latency_threads_${threads}.log"  
+    
+    rm "$logfile"
+
+    ./test_gds_latency 1 "$threads" ~/nvme0n1_geminifs/test_file.txt 
+    
+
+    echo "Run test_gds_cuFileRead success: threads=${threads}"  
+done  
+
